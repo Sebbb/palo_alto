8a8c1644b176f94f534ae71e5773b3d24eae39e9
Sebastian Roesner <sebastian.roesner@axa.com>
2026-06-09 23:10:40 +0200

    palo_alto/log.rb: Support Ruby 3

--- lib/palo_alto/log.rb
+++ lib/palo_alto/log.rb
@@ -3,13 +3,17 @@
 module PaloAlto
   # Palo Alto main class for XML
   class XML
-    def log(*args)
-      args.last.merge!({ client: self })
-      Log.new(*args)
+    def log(**args)
+      args.merge!({ client: self })
+      Log.new(**args)
     end
 
     # Palo Alto main class for log access
-    class Log < Enumerator
+    class Log
+      include Enumerable
+
+      attr_reader :count
+
       def initialize(client:, query:, log_type:, nlogs: 20, dir: :backward, show_detail: false, days: 7) # rubocop:disable Metrics/MethodLength,Metrics/ParameterLists
         @client = client
         @log_query_payload = {
@@ -21,17 +25,20 @@ module PaloAlto
           'show-detail': show_detail ? 'yes' : 'no'
         }
 
-        if days
-          @log_query_payload[:query] += " AND (receive_time geq '#{(Time.now - days * 3600 * 24).strftime('%Y/%m/%d %H:%M:%S')}')"
-        end
+        @log_query_payload[:query] =
+          [query, days ? "(receive_time geq #{(Time.now - (days * 3600 * 24)).to_i})" : nil].compact.join(' AND ')
 
         run_query
 
         @first_result = fetch_result
-        super
+        @enum = to_enum
       end
 
-      def run_query
+      def next
+        @enum.next
+      end
+
+      def run_query # rubocop:disable Metrics/MethodLength
         retried = false
         begin
           result = @client.execute(@log_query_payload)
@@ -49,17 +56,13 @@ module PaloAlto
         @skip = 0
       end
 
-      def restore_first
-        @current_result = @first_result
-        @skip = @current_result.at_xpath('response/result/log/logs/@count').value.to_i
-      end
-
       def rewind
-        restore_first
-        super
+        @current_result = @first_result
+        @skip = 0
+        @enum = to_enum
       end
 
-      def fetch_result # rubocop:disable Metrics/MethodLength
+      def fetch_result # rubocop:disable Metrics/MethodLength,Metrics/AbcSize,Metrics/CyclomaticComplexity
         loop_running = false
         return nil if @count && @skip == @count
 
@@ -92,7 +95,6 @@ module PaloAlto
 
         @count = @current_result.at_xpath('response/result/job/cached-logs').text.to_i
 
-        @skip += @current_result.at_xpath('response/result/log/logs/@count').value.to_i # skip now shown logs
         @current_result
       ensure
         if loop_running
@@ -105,12 +107,12 @@ module PaloAlto
         end
       end
 
-      attr_reader :count
+      def each # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
+        if @skip.positive? # in case #each is called again, e.g. because #first is called, reset all
+          @current_result = @first_result
+          @skip = 0
+        end
 
-      def each(&block) # rubocop:disable Metrics/MethodLength
-        # a bit buggy: after #to_a, without calling #rewind, I can't use #next reliable anymore
-
-        restore_first if @skip.positive?
         loop do
           @current_result.xpath('/response/result/log/logs/entry').each do |l|
             result = l.children.each_with_object({}) do |child, h|
@@ -118,8 +120,12 @@ module PaloAlto
 
               h[child.name] = child.text
             end
-            block.call(result)
+            yield result
           end
+
+          # skip already returned logs next time
+          @skip += @current_result.at_xpath('response/result/log/logs/@count').value.to_i
+
           break unless fetch_result
         end
       end
