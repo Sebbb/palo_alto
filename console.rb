86ac09d5be3e1fe2e67ad0b2b63012fbd49acaa9
Sebastian Roesner <sebastian.roesner@axa.com>
2026-06-11 03:36:31 +0200

    Update to newer gem versions

--- console.rb
+++ console.rb
@@ -6,14 +6,18 @@ Bundler.require(:default, :development)
 
 Dotenv.load('.env')
 
-require_relative './lib/palo_alto'
+require_relative 'lib/palo_alto'
 
 client = PaloAlto::XML.new(
-  host: ENV['PALO_ALTO_HOST'],
-  username: ENV['PALO_ALTO_USER'],
-  password: ENV['PALO_ALTO_PASSWORD'],
+  host: ENV.fetch('PALO_ALTO_HOST', nil),
+  username: ENV.fetch('PALO_ALTO_USER', nil),
+  password: ENV.fetch('PALO_ALTO_PASSWORD', nil),
   debug: ENV['PALO_ALTO_DEBUG']&.split(/\s/)&.map(&:to_sym) || []
 )
 
+IRB.setup(nil)
+workspace = IRB::WorkSpace.new(binding)
+irb = IRB::Irb.new(workspace)
 
-IRB.start
+IRB.conf[:MAIN_CONTEXT] = irb.context
+irb.eval_input
